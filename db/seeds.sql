7f59bb16c4356e226125363844ad55c668293de6
Hannah Jacoby <hjacoby@grubhub.com>
2019-09-17 18:52:40 -0600

    structure setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger, yes")
+INSERT INTO burgers (burger_name, devoured) VALUES ("hamburger, yes")
+INSERT INTO burgers (burger_name, devoured) VALUES (" bacon cheeseburger, yes")
+
+
