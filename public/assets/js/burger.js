d95a01cac883c16b2cf8127986ed52a4c655c3dd
Hannah Jacoby <hjacoby@grubhub.com>
2019-09-21 12:51:58 -0600

    handlebars code

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,24 +1,53 @@
 console.log("loaded");
+
 $(function() {
 $(".button").on("click",function(event) {
-    event.preventDefault();
+  event.preventDefault();
 console.log("submitted");
 var input = $("#burger").val()
 console.log(input)
-var newBurger = {
-    burger_name: input,
-    devoured: 0,
-}
-$.ajax("/api/burger", {
+
+// var newBurger = {
+//     burger_name: input,
+//     devoured: $("[name=devoured]:checked").val().trim()
+  var newDevoured = {
+    devoured: newDevoured
+  };
+
+
+$.ajax("/api/burgers/" + id, {
+  type: "PUT",
+  data: newDevoured
+}).then(
+  function() {
+    console.log("devoured", newDevoured);
+    location.reload();
+  }
+);
+
+$(".create-form").on("submit", function(event) {
+  // Make sure to preventDefault on a submit event.
+  event.preventDefault();
+
+  var newBurger = {
+    burger_name: $("#burger").val().trim(),
+    devoured: $("[name=devoured]:checked").val().trim()
+  };
+  $.ajax("/api/burger", {
     type: "POST",
     data: newBurger
   }).then(
     function() {
       console.log("new burger");
-      // Reload the page to get the updated list
       location.reload();
-    }
+    } 
   );
+  });
+  
+});
 
-})
-})
+
+
+
+
+});
