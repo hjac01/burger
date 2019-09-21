90d78fdf83e9d85e0ae59ff3f627fa98e7c71b36
Hannah Jacoby <hjacoby@grubhub.com>
2019-09-21 14:23:58 -0600

    button added

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,53 +1,42 @@
 console.log("loaded");
-
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-$(".button").on("click",function(event) {
-  event.preventDefault();
-console.log("submitted");
-var input = $("#burger").val()
-console.log(input)
-
-// var newBurger = {
-//     burger_name: input,
-//     devoured: $("[name=devoured]:checked").val().trim()
-  var newDevoured = {
-    devoured: newDevoured
-  };
-
-
-$.ajax("/api/burgers/" + id, {
-  type: "PUT",
-  data: newDevoured
-}).then(
-  function() {
-    console.log("devoured", newDevoured);
-    location.reload();
-  }
-);
-
-$(".create-form").on("submit", function(event) {
-  // Make sure to preventDefault on a submit event.
-  event.preventDefault();
-
-  var newBurger = {
-    burger_name: $("#burger").val().trim(),
-    devoured: $("[name=devoured]:checked").val().trim()
-  };
-  $.ajax("/api/burger", {
-    type: "POST",
-    data: newBurger
-  }).then(
-    function() {
-      console.log("new burger");
-      location.reload();
-    } 
-  );
-  });
+  $("ul").on("click", ".change-devoured", function(event) {
+    event.preventDefault();
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("newdevoured");
+    console.log("click")
   
-});
 
+    // Send the PUT request.
+    $.ajax("/api/burger/" + id, {
+      type: "PUT"
+    }).then(
+      function() {
+        console.log(id);
+        location.reload();
+      }
+    );
+  });
 
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
 
+    var newBurger = {
+      burger_name: $("#burger").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
 
+    // Send the POST request.
+    $.ajax("/api/burger/", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("new burger created");
+        location.reload();
+      }
+    );
+  });
 
 });
